437d992fa99f666eed79a341bc36cf3d526d2491
llowii <64591090+llowii@users.noreply.github.com>
2020-05-25 12:29:45 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+$('.hamburger').click(function(){
+
+        
+        $('#span1').toggleClass('active');
+        $('#span2').toggleClass('active');
+        $('#span3').toggleClass('active');
+        $('.right').toggleClass('active');
+        $('.link').toggleClass('active');
+        
+        
+    })
+    
+ 
+ $('.search').click(function(){
+  $('.search-btn').toggleClass('active');
+  
+  
+  
+ })
+ 
+  
+ 
