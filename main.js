78bcc9f735766dd15a70b187f3d7968f7ec3e2ae
llowii <64591090+llowii@users.noreply.github.com>
2020-05-27 19:57:00 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+$('.hamburger').click(function(){
+
+        
+        $('#span1').toggleClass('active');
+        $('#span2').toggleClass('active');
+        $('#span3').toggleClass('active');
+        $('.right').toggleClass('active');
+        $('.link').toggleClass('active');
+        
+        
+    })
+    $('#dark').click(function(){
+     $('body').toggleClass('yes');
+     $('#chemin').toggleClass('yes');
+     $('h1').toggleClass('yes');
+     $('.right').toggleClass('yes');
+     $('.ing').toggleClass('yes');
+     $('.step1').toggleClass('yes');
+     $('.step2').toggleClass('yes');
+     $('.step3').toggleClass('yes');
+     $('.step4').toggleClass('yes');
+     $('.step5').toggleClass('yes');
+     $('.step6').toggleClass('yes');
+     $('.step7').toggleClass('yes');
+     $('.step8').toggleClass('yes');
+     $('.step9').toggleClass('yes');
+     $('.search-txt').toggleClass('yes');
+     $('#dark').toggleClass('yes');
+     
+     
+    })
+     
+    
+ 
+ 
+ 
+  
+ 
