6ec7fd4984577ac379b229515208b563c7c223da
llowii <64591090+llowii@users.noreply.github.com>
2020-06-30 18:00:04 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,111 @@
+body {
+ background: #fffafa;
+ margin: 0;
+ padding: 0;
+ transition: 1s;
+
+ min-height: 100vh;
+ flex-direction: column;
+ font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+}
+
+header {
+ width: 100%;
+
+ height: 60px;
+ z-index: 3;
+}
+.pic,
+.ing,
+.step1,
+.step2,
+.step3,
+.step4,
+.step5,
+.step6,
+.step7,
+.step8,
+.step9 {
+ max-width: 500px;
+ font-weight: 450;
+}
+
+#dark {
+ color: #141414;
+ text-align: right;
+ float: right;
+ margin-right: 5%;
+ font-size: 15px;
+ transition: 1s;
+
+}
+
+#dark.yes {
+ color: #deeaf6;
+ transition: 1s;
+}
+
+
+body.yes,
+.right.yes {
+ background: #141414;
+ transition: 1s;
+}
+
+#chemin.yes,
+h1.yes {
+ color: #deeaf6;
+ transition: 1s;
+}
+
+
+.ing.yes,
+.info.yes,
+.step1.yes,
+.step2.yes,
+.step3.yes,
+.step4.yes,
+.step5.yes,
+.step6.yes,
+.step7.yes,
+.step8.yes,
+.step9.yes {
+ color: #fffafa;
+ background: #141414;
+ box-shadow: 10px 10px 10px #000000, -2.5px -2.5px 5px #6d6d6d;
+ transition: 1s;
+
+}
+.un.yes,.deux.yes,.trois.yes,.quatre.yes,.all.yes{
+ color: #fffafa;
+ background: #141414;
+ box-shadow: 1px 1px 10px #000000,
+ -1px -1px 5px #6d6d6d;
+ transition: 1s;}
+
+
+.stars {
+ text-align: center;
+ color: #02c39a;
+ margin-bottom: 25px;
+ margin-top: 0px;
+}
+
+.menu {
+ background-color: #02c39a;
+ display: block;
+ width: 100%;
+ z-index: 2;
+ position: fixed;
+}
+
+
+.icon {
+ margin: 0 7.5px;
+ text-decoration: none;
+ color: white;
+ font-size: 20px;
+}
+
+.center {
+ font-family: 'Chango', cur
